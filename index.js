87d48991b771c8f430dbb5930965e57cf5ee7ac3
Jason Serafica <seraficaj@gmail.com>
2022-02-25 09:55:44 -0800

    change page title

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const ejsLayouts = require("express-ejs-layouts");
 const app = express();
 const port = process.env.PORT || 4000;
 
+const db = require("./models");
+
 //Middleware
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 // Routes
-app.get('/', (req,res) => {
-    res.render("index")
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.get('/users/new', (req,res) => {
+app.get("/users/new", (req, res) => {
     res.render("users/new");
+});
+
+app.post("/users/new", (req, res) => {
+    db.user.findOrCreate({
+        
+    })
+    res.redirect("/");
 });
 
 app.listen(port, () => {
